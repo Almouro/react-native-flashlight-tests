2142cea4055a6adf759bd2bce4af70611c5b5cde
Ivan Robert <ivanr@bam.tech>
2023-12-06 16:29:43 +0100

    fix: use axios to fetch history

--- dailyBuild/fetchVersionsHistory.ts
+++ dailyBuild/fetchVersionsHistory.ts
@@ -1,18 +1,18 @@
+import axios from "axios";
 import { HistoryResult } from "./types";
 
-const fetchHistory = async (): Promise<unknown> => {
+const fetchHistory = async (): Promise<HistoryResult> => {
   console.log("fetching test history...");
   try {
-    const res = await fetch(
-      "https://us-central1-bam-flashlight.cloudfunctions.net/api/trpc/getUserRunHistory?batch=1&input=%7B%220%22%3A%7B%22projectId%22%3A%2261582a54-5f3b-44bb-8323-dd83e5ddef51%22%2C%22testName%22%3A%22pokedex-4-columns%20-%20Old%20arch%22%7D%7D",
-      { method: "GET" }
+    const res = await axios.get(
+      "https://us-central1-bam-flashlight.cloudfunctions.net/api/trpc/getUserRunHistory?batch=1&input=%7B%220%22%3A%7B%22projectId%22%3A%2261582a54-5f3b-44bb-8323-dd83e5ddef51%22%2C%22testName%22%3A%22pokedex-4-columns%20-%20Old%20arch%22%7D%7D"
     );
-    const result = await res.json();
 
-    return result;
+    return res.data;
   } catch (error) {
     console.error(error);
   }
+  throw new Error("An unexpected error occured while fetching history");
 };
 
 const adaptHistoryToVersions = (history: HistoryResult): string[] => {
@@ -24,6 +24,6 @@ const adaptHistoryToVersions = (history: HistoryResult): string[] => {
 };
 
 export const fetchVersionsHistory = async (): Promise<string[]> => {
-  const historyData = (await fetchHistory()) as HistoryResult;
+  const historyData = await fetchHistory();
   return adaptHistoryToVersions(historyData);
 };
