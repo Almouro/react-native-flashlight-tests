bec5f5940b4b7c8dd7c01292eee7c5045b89f28a
Ivan Robert <ivanr@bam.tech>
2023-12-06 14:40:22 +0100

    feat: compute missing version for tests

---
+++ dailyBuild/computeMissingVersions.ts
@@ -0,0 +1,60 @@
+//@ts-ignore
+const { exec } = require("child_process");
+import { fetchVersionsHistory } from "./dailyBuild";
+
+const fetchAvailableVersions = (): Promise<string[]> => {
+  return new Promise((resolve, reject) => {
+    exec(
+      "npm show react-native versions --json",
+      (err: string, stdout: string[], stderr: string) => {
+        if (err) {
+          reject(`exec error: ${err}`);
+          return;
+        }
+
+        if (stderr) {
+          reject(`stderr: ${stderr}`);
+          return;
+        }
+
+        try {
+          const versions = stdout;
+          resolve(versions);
+        } catch (parseError) {
+          reject(`JSON parse error: ${parseError}`);
+        }
+      }
+    );
+  });
+};
+
+export const computeMissingVersions = async (): Promise<string[]> => {
+  const computedVersions = await fetchVersionsHistory();
+
+  const availableVersions = await fetchAvailableVersions();
+
+  console.log("identifying new versions...");
+
+  const indexOfLastComputedInAvailable = availableVersions.indexOf(
+    computedVersions[computedVersions.length - 1]
+  );
+
+  if (computedVersions.length === 0 || availableVersions.length === 0) {
+    return [];
+  }
+
+  if (indexOfLastComputedInAvailable === -1) {
+    // Handle the case where the last computed version isn't in available versions
+    return [];
+  }
+
+  const versionsAfterLastComputed = availableVersions.slice(
+    indexOfLastComputedInAvailable + 1
+  );
+
+  console.log("untested versions were identified");
+
+  return versionsAfterLastComputed;
+};
+
+computeMissingVersions();
--- dailyBuild/dailyBuild.ts
+++ dailyBuild/dailyBuild.ts
@@ -1,6 +1,7 @@
 import { HistoryResult } from "./types";
 
 const fetchHistory = async (): Promise<unknown> => {
+  console.log("fetching test history...");
   try {
     const res = await fetch(
       "https://us-central1-bam-flashlight.cloudfunctions.net/api/trpc/getUserRunHistory?batch=1&input=%7B%220%22%3A%7B%22projectId%22%3A%2261582a54-5f3b-44bb-8323-dd83e5ddef51%22%2C%22testName%22%3A%22pokedex-4-columns%20-%20Old%20arch%22%7D%7D",
@@ -18,8 +19,7 @@ const adaptHistoryToVersions = (history: HistoryResult): string[] => {
   return history[0].result.data.map((data) => data.tag.name);
 };
 
-const fetchVersionsHistory = async (): Promise<string[]> => {
+export const fetchVersionsHistory = async (): Promise<string[]> => {
   const historyData = (await fetchHistory()) as HistoryResult;
   return adaptHistoryToVersions(historyData);
 };
-fetchVersionsHistory();
