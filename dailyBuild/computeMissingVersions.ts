d04d7de1fa155f3b0ef25ca95786a98ab1ab5d92
Ivan Robert <ivanr@bam.tech>
2023-12-07 14:57:13 +0100

    fix: build for missing versions instead of versions after the last tested

--- dailyBuild/computeMissingVersions.ts
+++ dailyBuild/computeMissingVersions.ts
@@ -28,32 +28,40 @@ const fetchAvailableVersions = (): Promise<string[]> => {
   });
 };
 
-export const computeMissingVersions = async (): Promise<string[]> => {
+export const computeMissingVersions = async (
+  startVersion: string
+): Promise<string[]> => {
   const computedVersions = await fetchVersionsHistory();
 
-  const availableVersions = await fetchAvailableVersions();
+  const allVersions = await fetchAvailableVersions();
+
+  if (!allVersions.includes(startVersion)) {
+    throw new Error(
+      `startVersion ${startVersion} is not available in npm registry`
+    );
+  }
+
+  const availableVersions = allVersions.slice(
+    allVersions.indexOf(startVersion)
+  );
+
+  const missingVersions: string[] = [];
 
   console.log("identifying new versions...");
 
-  const indexOfLastComputedInAvailable = availableVersions.indexOf(
-    computedVersions[computedVersions.length - 1]
-  );
-
-  if (computedVersions.length === 0 || availableVersions.length === 0) {
-    return [];
+  for (const version of availableVersions) {
+    if (!computedVersions.includes(version)) {
+      missingVersions.push(version);
+    }
   }
 
-  if (indexOfLastComputedInAvailable === -1) {
-    // Handle the case where the last computed version isn't in available versions
+  if (missingVersions.length === 0) {
+    console.log("no new version to compute");
     return [];
   }
-
-  const versionsAfterLastComputed = availableVersions.slice(
-    indexOfLastComputedInAvailable + 1
-  );
 
   console.log("untested versions were identified");
-  console.log("versions found :", versionsAfterLastComputed);
+  console.log("versions found :", missingVersions);
 
-  return versionsAfterLastComputed;
+  return missingVersions;
 };
