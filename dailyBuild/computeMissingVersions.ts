3da0eaf15e7011a15c8840b9948bab328eb45de7
Ivan Robert <ivanr@bam.tech>
2023-12-07 15:36:12 +0100

    refactor(daily build): extract fetchAvailableVersions

--- dailyBuild/computeMissingVersions.ts
+++ dailyBuild/computeMissingVersions.ts
@@ -1,32 +1,5 @@
-//@ts-ignore
-const { exec } = require("child_process");
+import { fetchAvailableVersions } from "./fetchAvailableVersions";
 import { fetchVersionsHistory } from "./fetchVersionsHistory";
-
-const fetchAvailableVersions = (): Promise<string[]> => {
-  return new Promise((resolve, reject) => {
-    exec(
-      "npm show react-native versions --json",
-      (err: string, stdout: string, stderr: string) => {
-        if (err) {
-          reject(`exec error: ${err}`);
-          return;
-        }
-
-        if (stderr) {
-          reject(`stderr: ${stderr}`);
-          return;
-        }
-
-        try {
-          const versions = stdout;
-          resolve(JSON.parse(versions));
-        } catch (parseError) {
-          reject(`JSON parse error: ${parseError}`);
-        }
-      }
-    );
-  });
-};
 
 export const computeMissingVersions = async (
   startVersion: string
