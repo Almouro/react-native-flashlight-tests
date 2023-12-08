a86781358c001616602f78cdfce46fdbf8a7fc61
Ivan Robert <ivanr@bam.tech>
2023-12-08 17:51:33 +0100

    refactor: clean API call to fetch history

---
+++ dailyBuild/constants.ts
@@ -0,0 +1,5 @@
+export const BASE_API_URL =
+  "https://us-central1-bam-flashlight.cloudfunctions.net/api/";
+export const URL_OPTIONS = "trpc/getUserRunHistory";
+export const RN_TEST_PROJECT_ID = "61582a54-5f3b-44bb-8323-dd83e5ddef51";
+export const TEST_NAME = "pokedex-4-columns - Old arch";
