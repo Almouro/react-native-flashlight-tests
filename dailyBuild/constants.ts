df26f049948a98fe0e05db8dae0b4fa342030f1d
almouro <contact@almouro.com>
2024-01-10 12:34:09 +0100

    change scenario to have better standard deviation

--- dailyBuild/constants.ts
+++ dailyBuild/constants.ts
@@ -4,5 +4,5 @@ export const URL_OPTIONS = "trpc/getUserRunHistory";
 export const RN_TEST_PROJECT_ID = "61582a54-5f3b-44bb-8323-dd83e5ddef51";
 export const TEST_NAME = "pokedex-4-columns_Old-arch";
 export const SCENARIO = "pokedex-4-columns";
-export const START_VERSION = "0.69.9";
+export const START_VERSION = "0.74.0-nightly-20240108-05ca2c6d8";
 export const MAXIMUM_VERSIONS_TO_BUILD = 15;
--- dailyBuild/userRunHistory/userRunHistory.api.ts
+++ dailyBuild/userRunHistory/userRunHistory.api.ts
@@ -1,55 +1,41 @@
 import axios from "axios";
 import { HistoryResult } from "./types";
-import {
-  BASE_API_URL,
-  RN_TEST_PROJECT_ID,
-  TEST_NAME,
-  URL_OPTIONS,
-} from "../constants";
+import { BASE_API_URL, RN_TEST_PROJECT_ID, URL_OPTIONS } from "../constants";
 
 export const fetchHistory = async (
+  testName: string,
   numberOfTests: number
 ): Promise<HistoryResult> => {
   console.log("fetching test history...");
-  try {
-    const params = {
-      batch: 1,
-      input: JSON.stringify({
-        "0": {
-          projectId: RN_TEST_PROJECT_ID,
-          testName: TEST_NAME,
-          pageSize: numberOfTests,
-        },
-      }),
-    };
+  const params = {
+    batch: 1,
+    input: JSON.stringify({
+      "0": {
+        projectId: RN_TEST_PROJECT_ID,
+        testName,
+        pageSize: numberOfTests,
+      },
+    }),
+  };
 
-    const res = await axios.get(`${BASE_API_URL}${URL_OPTIONS}`, { params });
-    return res.data;
-  } catch (error) {
-    console.error(error);
-  }
-  throw new Error("An unexpected error occured while fetching history");
+  const res = await axios.get(`${BASE_API_URL}${URL_OPTIONS}`, { params });
+  return res.data;
 };
 
-export const fetchNumberOfTests = async (): Promise<number> => {
+export const fetchNumberOfTests = async (testName: string): Promise<number> => {
   console.log("fetching number of tests...");
-  try {
-    const params = {
-      batch: 1,
-      input: JSON.stringify({
-        "0": {
-          projectId: RN_TEST_PROJECT_ID,
-          testName: TEST_NAME,
-          pageSize: 1,
-        },
-      }),
-    };
+  const params = {
+    batch: 1,
+    input: JSON.stringify({
+      "0": {
+        projectId: RN_TEST_PROJECT_ID,
+        testName,
+        pageSize: 1,
+      },
+    }),
+  };
 
-    const res = await axios.get(`${BASE_API_URL}${URL_OPTIONS}`, { params });
+  const res = await axios.get(`${BASE_API_URL}${URL_OPTIONS}`, { params });
 
-    return res.data[0].result.data.totalCount;
-  } catch (error) {
-    console.error(error);
-  }
-  throw new Error("An unexpected error occured while fetching history");
+  return res.data[0].result.data.totalCount;
 };
