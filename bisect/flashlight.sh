21a9e06a58c0f6927bc8f1d99a5b3ffce2a13367
almouro <contact@almouro.com>
2024-01-19 14:31:25 +0100

    improve bisect

--- bisect/flashlight.sh
+++ bisect/flashlight.sh
@@ -11,10 +11,18 @@ commit_hash=$(git rev-parse HEAD)
 commit_date=$(git show -s --format=%cI ${commit_hash})
 cd ..
 
-# See https://app.flashlight.dev/projects/c0fd02d4-bfcf-4a27-aef4-edf82bd399f4/test-list
-# https://app.flashlight.dev/projects/c0fd02d4-bfcf-4a27-aef4-edf82bd399f4/evolution?testName=pok4col
-PROJECT_ID="c0fd02d4-bfcf-4a27-aef4-edf82bd399f4"
+# See https://app.flashlight.dev/projects/878cc105-8655-4f3e-8926-a6186f3ff0b8/test-list
+# https://app.flashlight.dev/projects/878cc105-8655-4f3e-8926-a6186f3ff0b8/evolution?testName=pok4col
+PROJECT_ID="878cc105-8655-4f3e-8926-a6186f3ff0b8"
 
 curl https://get.flashlight.dev | bash
 # For some reason path overriding doesn't work
-/home/runner/.flashlight/bin/flashlight cloud --test bisect/start.yaml --duration 10000 --app "${commit_hash}-pok4col.apk" --projectId $PROJECT_ID --tagName $commit_hash --tagDate $commit_date --testName "pok4col"
+/home/runner/.flashlight/bin/flashlight cloud \
+  --test bisect/start.yaml \
+  --beforeAll bisect/start.yaml \
+  --duration 10000 \
+  --app "${commit_hash}-pok4col.apk" \
+  --projectId $PROJECT_ID \
+  --tagName $commit_hash \
+  --tagDate $commit_date \
+  --testName "pok4col"
