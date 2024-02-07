f7ba97e7408db12e1511966ef2d50c67959a8b2e
almouro <contact@almouro.com>
2024-02-07 15:17:58 +0000

    add some logs for flatlist

--- scenarios/FlatListExample/App.tsx
+++ scenarios/FlatListExample/App.tsx
@@ -1,4 +1,4 @@
-import React, {memo} from 'react';
+import React, { memo } from "react";
 import {
   FlatList,
   Image,
@@ -8,45 +8,45 @@ import {
   Text,
   useColorScheme,
   View,
-} from 'react-native';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
-import {images} from './imageMap.js';
+} from "react-native";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+import { images } from "./imageMap.js";
 
 interface Pokemon {
   gameIndex: string;
   name: string;
-  types: {name: string}[];
+  types: { name: string }[];
   imageUrl: string;
 }
 
 // Taken directly from https://github.com/MatheusPires99/pokedex
-const POKEMON_TYPE_COLORS: {[type: string]: string} = {
-  normal: '#A8A878',
-  fighting: '#C03028',
-  flying: '#A890F0',
-  poison: '#A040A0',
-  ground: '#E0C068',
-  rock: '#B8A038',
-  bug: '#A8B820',
-  ghost: '#705898',
-  steel: '#B8B8D0',
-  fire: '#FA6C6C',
-  water: '#6890F0',
-  grass: '#48CFB2',
-  electric: '#FFCE4B',
-  psychic: '#F85888',
-  ice: '#98D8D8',
-  dragon: '#7038F8',
-  dark: '#705848',
-  fairy: '#EE99AC',
+const POKEMON_TYPE_COLORS: { [type: string]: string } = {
+  normal: "#A8A878",
+  fighting: "#C03028",
+  flying: "#A890F0",
+  poison: "#A040A0",
+  ground: "#E0C068",
+  rock: "#B8A038",
+  bug: "#A8B820",
+  ghost: "#705898",
+  steel: "#B8B8D0",
+  fire: "#FA6C6C",
+  water: "#6890F0",
+  grass: "#48CFB2",
+  electric: "#FFCE4B",
+  psychic: "#F85888",
+  ice: "#98D8D8",
+  dragon: "#7038F8",
+  dark: "#705848",
+  fairy: "#EE99AC",
 };
 
-const pokedex: Pokemon[] = require('./pokedex.json');
+const pokedex: Pokemon[] = require("./pokedex.json");
 
 const COLUMN_COUNT = 4;
 
 const cardStyles = StyleSheet.create({
-  container: {width: `${100 / COLUMN_COUNT}%`},
+  container: { width: `${100 / COLUMN_COUNT}%` },
   innerContainer: {
     padding: (10 * 2) / COLUMN_COUNT,
     paddingRight: 0,
@@ -56,41 +56,51 @@ const cardStyles = StyleSheet.create({
   },
   name: {
     fontSize: (16 * 2) / COLUMN_COUNT,
-    color: 'white',
-    fontWeight: 'bold',
+    color: "white",
+    fontWeight: "bold",
     flex: 1,
     paddingRight: (5 * 2) / COLUMN_COUNT,
   },
   typeContainer: {
-    width: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#6662',
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#6662",
     borderRadius: (10 * 2) / COLUMN_COUNT,
     marginTop: (5 * 2) / COLUMN_COUNT,
     padding: (5 * 2) / COLUMN_COUNT,
   },
   nameAndIndexContainer: {
-    flexDirection: 'row',
-    width: '100%',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
     paddingRight: (10 * 2) / COLUMN_COUNT,
   },
-  index: {fontSize: (12 * 2) / COLUMN_COUNT, color: '#6666'},
+  index: { fontSize: (12 * 2) / COLUMN_COUNT, color: "#6666" },
   type: {
-    color: '#fffa',
+    color: "#fffa",
     fontSize: (12 * 2) / COLUMN_COUNT,
   },
-  image: {width: (100 * 2) / COLUMN_COUNT, height: (100 * 2) / COLUMN_COUNT},
-  typesContainer: {flex: 1, paddingTop: (20 * 2) / COLUMN_COUNT},
+  image: { width: (100 * 2) / COLUMN_COUNT, height: (100 * 2) / COLUMN_COUNT },
+  typesContainer: { flex: 1, paddingTop: (20 * 2) / COLUMN_COUNT },
   typeRow: {
-    flexDirection: 'row',
-    width: '100%',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
   },
 });
 
-const PokemonCard = memo(({item}: {item: Pokemon}) => {
+let renderMemoCount = 0;
+
+const time = new Date().getTime();
+
+const PokemonCard = memo(({ item }: { item: Pokemon }) => {
+  renderMemoCount += 1;
+
+  if (renderMemoCount === 448) {
+    console.log("[FLASHLIGHT] Last render memo", new Date().getTime() - time);
+  }
+
   return (
     <View style={cardStyles.container}>
       <View
@@ -99,7 +109,8 @@ const PokemonCard = memo(({item}: {item: Pokemon}) => {
           {
             backgroundColor: POKEMON_TYPE_COLORS[item.types[0].name],
           },
-        ]}>
+        ]}
+      >
         <View style={cardStyles.nameAndIndexContainer}>
           <Text numberOfLines={1} style={cardStyles.name}>
             {item.name.toUpperCase()}
@@ -108,7 +119,7 @@ const PokemonCard = memo(({item}: {item: Pokemon}) => {
         </View>
         <View style={cardStyles.typeRow}>
           <View style={cardStyles.typesContainer}>
-            {item.types.map(type => (
+            {item.types.map((type) => (
               <View key={type.name} style={cardStyles.typeContainer}>
                 <Text style={cardStyles.type}>{type.name}</Text>
               </View>
@@ -119,8 +130,8 @@ const PokemonCard = memo(({item}: {item: Pokemon}) => {
               source={
                 images[
                   item.imageUrl.replace(
-                    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/',
-                    '',
+                    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/",
+                    ""
                   )
                 ]
               }
@@ -133,14 +144,28 @@ const PokemonCard = memo(({item}: {item: Pokemon}) => {
   );
 });
 
-const renderItem = ({item}: {item: Pokemon}) => {
+let renderItemCount = 0;
+
+const renderItem = ({ item }: { item: Pokemon }) => {
+  renderItemCount += 1;
   return <PokemonCard item={item} />;
 };
 
-const styles = StyleSheet.create({list: {paddingHorizontal: 5}});
+const styles = StyleSheet.create({ list: { paddingHorizontal: 5 } });
+
+setTimeout(
+  () =>
+    console.log("[FLASHLIGHT] 10s mark", { renderItemCount, renderMemoCount }),
+  10000
+);
+setTimeout(
+  () =>
+    console.log("[FLASHLIGHT] 30s mark", { renderItemCount, renderMemoCount }),
+  30000
+);
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const isDarkMode = useColorScheme() === "dark";
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
@@ -148,11 +173,11 @@ const App = () => {
 
   return (
     <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <StatusBar barStyle={isDarkMode ? "light-content" : "dark-content"} />
 
       <FlatList
         numColumns={COLUMN_COUNT}
-        keyExtractor={item => item.name}
+        keyExtractor={(item) => item.name}
         /**
          * Slicing here to make sure we always render the same amount of items
          * It seems that on certain iterations FlatList would render one more page and have a worse Flashlight score
