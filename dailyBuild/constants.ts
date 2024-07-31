a9a0ec0aea415b1769bdd1b3aac4584b1c590aac
almouro <contact@almouro.com>
2024-07-31 09:32:37 +0200

    chore: add failing versions

--- dailyBuild/constants.ts
+++ dailyBuild/constants.ts
@@ -14,6 +14,8 @@ export const IGNORED_VERSIONS = [
   "0.72.0-rc.3", // build crashing for both arch
   "0.74.0-nightly-20231110-9b33e752c", // build crashing for new arch
   "0.74.0-nightly-20240212-2090fe5cf", // build failing for new arch
+  "0.75.0-rc.0", // build failing for both arch
+  "0.75.0-rc.1", // build failing for both arch
   "1000.0.0",
 ];
 export const MAXIMUM_VERSIONS_TO_BUILD = 15;
